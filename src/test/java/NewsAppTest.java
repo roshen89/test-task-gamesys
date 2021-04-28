19b6b204fdd8a39849e6be96aadb40ab688b664f
ROSH <rovshen.quliyev@voidaint.com>
2021-04-28 15:15:07 +0300

    - NewsAppTest

---
+++ src/test/java/NewsAppTest.java
@@ -0,0 +1,12 @@
+import com.buzzfeed.NewsApp;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = NewsApp.class)
+class NewsAppTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
