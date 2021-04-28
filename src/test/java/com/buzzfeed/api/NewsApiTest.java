8b2a7dcf479d62aa1f7b3ffe3c0feeb00f31dc2d
ROSH <rovshen.quliyev@voidaint.com>
2021-04-28 08:59:14 +0300

    - Add NewsApiTest

---
+++ src/test/java/com/buzzfeed/api/NewsApiTest.java
@@ -0,0 +1,57 @@
+package com.buzzfeed.api;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.buzzfeed.data.NewsDTO;
+import com.buzzfeed.data.RestResponse;
+import com.buzzfeed.service.NewsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringRunner.class)
+public class NewsApiTest {
+
+  @Mock
+  private NewsService service;
+
+  @InjectMocks
+  private NewsApi api;
+
+  private ObjectMapper mapper;
+  private MockMvc mockMvc;
+
+  @Before
+  public void setUp() {
+    this.mockMvc = MockMvcBuilders.standaloneSetup(api).build();
+    this.mapper = new ObjectMapper();
+  }
+
+  @Test
+  public void testGetLastNews() throws Exception {
+    var list = List.of(new NewsDTO());
+    RestResponse<List<NewsDTO>> expected = new RestResponse<>(list);
+
+    when(service.getLastNews()).thenReturn(list);
+    MvcResult mvcResult = getLastNews();
+
+    assertThat(mapper.writeValueAsString(expected)).isEqualTo(mvcResult.getResponse().getContentAsString());
+  }
+
+  public MvcResult getLastNews() throws Exception {
+    return mockMvc.perform(MockMvcRequestBuilders.get("/news"))
+        .andExpect(status().isOk())
+        .andReturn();
+  }
+}
